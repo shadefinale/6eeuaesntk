e8ff2ef8e54786a97b42f43a3384aa5653953bfb
shadefinale <shadefinale@gmail.com>
2015-08-10 11:08:14 -0500

    Username length validation tests completed.

--- sneakers/spec/models/user_spec.rb
+++ sneakers/spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  context 'length validations' do
+    it 'should fail if username too short' do
+      new_user = build(:user, username: "11111")
+      expect(new_user).to be_invalid
+    end
+
+    it 'should fail if username not too short' do
+      new_user = build(:user, username: "111111")
+      expect(new_user).to be_valid
+    end
+
+    it 'should fail if username too long' do
+      new_user = build(:user, username: "1" * 21)
+      expect(new_user).to be_invalid
+    end
+
+    it 'should fail if username too long' do
+      new_user = build(:user, username: "1" * 20)
+      expect(new_user).to be_valid
+    end
+  end
 end
