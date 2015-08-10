feb6aef0b6d12ca3dd5ac788e5dea087a7f9c17a
shadefinale <shadefinale@gmail.com>
2015-08-10 10:26:20 -0500

    Working on adding session support.

---
+++ sneakers/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_username(params[:username])
+    if @user && @user.authenticate(params[:email])
+      sign_in(@user)
+      flash[:notice] = "You've successfully signed in!"
+      redirect_to @user
+    else
+      flash.now[:notice] = "Unable to sign in, please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:notice] = "You've been signed out."
+    redirect_to new_session_path
+  end
+end
+
