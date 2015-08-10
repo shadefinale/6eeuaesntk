340b228302a3645a1422dc70d46d4b5ae4628979
shadefinale <shadefinale@gmail.com>
2015-08-10 11:19:09 -0500

    User controller show specs done.

--- sneakers/spec/controllers/users_controller_spec.rb
+++ sneakers/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, :type => :controller do
+describe UsersController do
 
+  context '#show' do
+    let(:user) { create(:user) }
+    it 'should redirect to new user path if not logged in' do
+      create(:user)
+      get :show, id: 1
+      expect(response).to redirect_to(new_user_path)
+    end
+
+    it 'should raise error if no id passed' do
+      create(:user)
+      expect{get :show}.to raise_error
+
+    end
+  end
 end
