654759568958293f592e7da9ce7a51d6818920de
shadefinale <shadefinale@gmail.com>
2015-08-10 11:26:21 -0500

    Shoe happy/sad specs completed.

--- sneakers/app/controllers/shoes_controller.rb
+++ sneakers/app/controllers/shoes_controller.rb
@@ -10,7 +10,6 @@ class ShoesController < ApplicationController
       flash[:success] = "New shoes, my favorite!"
       redirect_to @shoe.user
     else
-      binding.pry
       flash.now[:error] = "You're probably missing something."
       render :new
     end
--- sneakers/spec/controllers/shoes_controller_spec.rb
+++ sneakers/spec/controllers/shoes_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe ShoesController, :type => :controller do
+describe ShoesController do
+  context 'shoes happy path' do
+    it 'should not redirect to new shoe path if valid' do
+      new_user = create(:user)
+      new_shoe = build(:shoe, user: new_user)
+      post :create, shoe: attributes_for(:shoe, user: new_user), user_id: new_user.id
+      expect(response).not_to redirect_to(new_user_shoe_path)
+    end
 
+    it 'should create a new shoe path if valid' do
+      new_user = create(:user)
+      new_shoe = build(:shoe, user: new_user)
+      expect do
+        post :create, shoe: attributes_for(:shoe, user: new_user), user_id: new_user.id
+      end.to change(Shoe, :count).by(1)
+    end
+  end
+
+  context 'shoes sad path' do
+    it 'should render new shoe template if invalid' do
+      new_user = create(:user)
+      post :create, shoe: attributes_for(:shoe, user: new_user, brand: ""), user_id: new_user.id
+      expect(response).to render_template(:new)
+    end
+
+    it 'should not create a new shoe path if invalid' do
+      new_user = create(:user)
+      new_shoe = build(:shoe, user: new_user)
+      expect do
+        post :create, shoe: attributes_for(:shoe, user: new_user, brand: ""), user_id: new_user.id
+      end.to change(Shoe, :count).by(0)
+    end
+  end
 end
